7f3907561329b5ba2716be1dddf0775cc0477fb8
unknown <mmreyes13@gmail.com>
2017-08-27 22:25:10 -0400

    Second commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,56 +1,55 @@
-// To make functions available when document is loaded //
-$(document).ready(function(){
+
 
 
 			// Array for the questions, choices and answers //
 			var trivia = [
 			{
-				question: "Which city has the highest rate of lightning strikes?",
+				question: "1. Which city has the highest rate of lightning strikes?",
 				choices: ["Orlando", "Miami", "Clearwater"],
 				answer: "Clearwater"
 			},
 			{
-				question: "Which city has the highest average temperature?",
+				question: "2. Which city has the highest average temperature?",
 				choices: ["Miami", "Orlando", "Key West"],
 				answer: "Key West"
 			},
 			{
-				question: "This river flows north instead of south",
+				question: "3. This river flows north instead of south",
 				choices: ["Saint John's River", "Matanzas River", "Saint Lucie River"],
 				answer: "Saint John's River"
 			},
 			{
-				question: "Ybor City was once known as the",
+				question: "4. Ybor City was once known as the",
 				choices: ["Beer capital of the world", "Cigar capital of the world", "Party capital of the world"],
 				answer: "Cigar capital of the world"
 			},
 			{
-				question: "Home to one of the two naturally round lakes in the world",
+				question: "5. Home to one of the two naturally round lakes in the world",
 				choices: ["DeFuniak Springs", "Wekiva Springs", "Silver Glen Springs"],
 				answer: "DeFuniak Springs"
 			},
 			{
-				question: "Known as the Venice of the Americas",
+				question: "6. Known as the Venice of the Americas",
 				choices: ["Cape Coral", "Fort Lauderdale", "Fort Myers"],
 				answer: "Fort Lauderdale"	
 			},
 			{
-				question: "Known as the Dive Capital of the World",
+				question: "7. Known as the Dive Capital of the World",
 				choices: ["Key West", "Key Largo", "Islamorada"],
 				answer: "Key Largo"
 			},
 			{
-				question: "Islamorada is billed as the",
+				question: "8. Islamorada is billed as the",
 				choices: ["Sports fishing capital of the world", "Red island", "Best place to jet ski"],
 				answer: "Sports fishing capital of the world"
 			},
 			{
-				question: "Florida is the only state that has",
+				question: "9. Florida is the only state that has",
 				choices: ["Two springs with the same name", "Two lakes with the same name", "Two rivers with the same name"],
 				answer: "Two rivers with the same name"
 			},
 			{
-				question: "The highest point in Florida is how many feet above sea level?",
+				question: "10. The highest point in Florida is how many feet above sea level?",
 				choices: ["345", "320", "307"],
 				answer: "345"
 			
@@ -63,6 +62,7 @@ $(document).ready(function(){
 			var counter = 100;
 			var clockRunning = false;
 			var timer;
+			var displayTrivia = $("#questions");
 
 			// Setting timer //
 			setTimeout(onehundredSeconds, 1000 * 100);
@@ -120,6 +120,84 @@ $(document).ready(function(){
 		
 //each line should be a sentence, click start and put everything on the screen
 //questions on screen
+  function beginGame() {
+    
+    $('#start').remove();
+
+
+    for (var i = 0; i < trivia.length; i++) {
+      displayTrivia.append('<h2>' + trivia[i].question + '</h2>');
+     
+      for (var a = 0; a < trivia[i].choices.length; a++) {
+        displayTrivia.append('<input type="radio" name="question' + '-' + i + '" value="' + trivia[i].choices[a] + '">' + trivia[i].choices[a]);
+      }
+    }
+
+    displayTrivia.append('<button id="finish">Finish</button>');
+  };
+   function finished() {
+
+
+    $.each($("input[name='question-0']:checked"), function() {
+      if ($(this).val() == trivia[0].choices) {
+        correct++;
+      } else {
+        incorrect++;
+      }
+    });
+    $.each($("input[name='question-1']:checked"), function() {
+        if ($(this).val() == trivia[1].choices) {
+        correct++;
+      } else {
+        incorrect++;
+      }
+    });
+    $.each($("input[name='question-2']:checked"), function() {
+      if ($(this).val() == trivia[2].choices) {
+        correct++;
+      } else {
+        incorrect++;
+      }
+    });
+    $.each($("input[name='question-3']:checked"), function() {
+      if ($(this).val() == trivia[3].choices) {
+        correct++;
+      } else {
+        incorrect++;
+      }
+    });
+    $.each($("input[name='question-4']:checked"), function() {
+      if ($(this).val() == trivia[4].choices) {
+        correct++;
+      } else {
+        incorrect++;
+      }
+    });
+    $.each($("input[name='question-5']:checked"), function() {
+      if ($(this).val() == trivia[5].choices) {
+        correct++;
+      } else {
+        incorrect++;
+      }
+    });
+    $.each($("input[name='question-6']:checked"), function() {
+      if ($(this).val() == trivia[6].choices) {
+        correct++;
+      } else {
+        incorrect++;
+      }
+    });
+    $.each($("input[name='question-7']:checked"), function() {
+      if ($(this).val() == trivia[7].choices) {
+        correct++;
+      } else {
+        incorrect++;
+      }
+    });
+};
+
+    beginGame();
+    finished();
 	//make a loop
 		//make div and put qestion in div
 		//add div to page
@@ -139,5 +217,3 @@ $(document).ready(function(){
 //Then check user's answers
 //Show correct and incorrect answer
 	
-
-});
